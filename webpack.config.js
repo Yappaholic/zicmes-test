a4b95ccae431cd6d7db716548c14afb92522f050
yappaholic <sav.boyar@gmail.com>
2024-12-18 17:46:57 +0300

    feat: Fixed catalogue gaps and made changes for mobile version

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
-    hot: true
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' })
