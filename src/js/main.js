0142557bb94290012beab95e57c4d65aa0c11647
yappaholic <sav.boyar@gmail.com>
2024-12-19 20:43:28 +0300

    feat: Added side menu for mobile and main cover resizing

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 import "../scss/styles.scss"
 import bootstrap from 'bootstrap'
+
+const catalogLink = document.querySelectorAll(".catalog-link");
+const catalog = document.querySelector("#catalog");
+
+catalogLink.forEach(anchor => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    catalog.scrollIntoView({behavior: "smooth"});    
+  });
+});
